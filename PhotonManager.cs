5d83fbb7dbc37a3f71b5883169853ddfe1953661
Howard Ham <away1597@gmail.com>
2019-07-03 15:25:01 +0900

    서버 접속 - 서버 접속 완료 알림 구현

--- PhotonManager.cs
+++ PhotonManager.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhotonManager : MonoBehaviour
+public class PhotonManager : Photon.PunBehaviour
 {
     public static PhotonManager instance;
     public static GameObject localPlayer;
-    public Text guideText;
     public GameObject joinBTN;
+    public Text guideText;
 
     public void Awake()
     {
@@ -18,12 +18,20 @@ public class PhotonManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        guideText.text = "connect to Photon..";
+        PhotonNetwork.ConnectUsingSettings("network_v1.0");
+        joinBTN.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        guideText.text = "Master Server에 연결되었습니다.";
+        joinBTN.SetActive(true);
     }
 }
