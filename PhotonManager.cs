3adbbfb92241e53ded3417a9d81ef901f2bfb3d4
Howard Ham <away1597@gmail.com>
2019-07-03 15:15:53 +0900

    second commit

--- PhotonManager.cs
+++ PhotonManager.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotonManager : MonoBehaviour
 {
+    public static PhotonManager instance;
+    public static GameObject localPlayer;
+    public Text guideText;
+    public GameObject joinBTN;
+
+    public void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
